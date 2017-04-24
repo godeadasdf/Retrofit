693a26cc3578db6b7d55330e09621c0199ee7086
kangweichen <kangweichen@feinno.com>
2017-04-24 10:39:23 +0800

    add FragmentPagerAdapter For MainActivity

--- app/src/main/java/com/b/kang/retrofit/MainActivity.java
+++ app/src/main/java/com/b/kang/retrofit/MainActivity.java
@@ -2,26 +2,58 @@ package com.b.kang.retrofit;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.b.kang.retrofit.adapter.HomePagerAdapter;
+import com.b.kang.retrofit.fragment.BaseFragment;
 import com.b.kang.retrofit.fragment.DailyFragment;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements ViewPager.OnPageChangeListener {
 
     private FragmentManager fm;
+
+    private ViewPager pager;
+    private HomePagerAdapter adapter;
+    private List<BaseFragment> fragments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initFragment();
+        initPager();
     }
 
-    private void initFragment(){
+    private void initPager(){
         fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.fragment_container,new DailyFragment());
-        ft.commit();
+        pager = (ViewPager)findViewById(R.id.pager);
+        fragments.add(new DailyFragment());
+        fragments.add(new DailyFragment());
+        fragments.add(new DailyFragment());
+        adapter = new HomePagerAdapter(fm,fragments);
+        pager.setAdapter(adapter);
+        pager.setCurrentItem(0);
+        pager.addOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        //// TODO: 17-4-24 need add reaction for bottom navigator style change 
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
--- app/src/main/java/com/b/kang/retrofit/adapter/HomePagerAdapter.java
+++ app/src/main/java/com/b/kang/retrofit/adapter/HomePagerAdapter.java
@@ -1,19 +1,40 @@
 package com.b.kang.retrofit.adapter;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
+
+import com.b.kang.retrofit.fragment.BaseFragment;
+
+import java.util.List;
 
 /**
  * Created by kang on 17-4-20.
  */
-public class HomePagerAdapter extends PagerAdapter {
-    @Override
-    public int getCount() {
-        return 0;
+public class HomePagerAdapter extends FragmentPagerAdapter {
+
+    public HomePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    private FragmentManager fragmetnmanager;  //创建FragmentManager
+    private List<BaseFragment> listfragment; //创建一个List<Fragment>
+
+    public HomePagerAdapter(FragmentManager fm,List<BaseFragment> list) {
+        super(fm);
+        this.fragmetnmanager=fm;
+        this.listfragment=list;
     }
 
     @Override
-    public boolean isViewFromObject(View view, Object object) {
-        return false;
+    public Fragment getItem(int arg0) {
+        return listfragment.get(arg0); //返回第几个fragment
+    }
+
+    @Override
+    public int getCount() {
+        return listfragment.size(); //总共有多少个fragment
     }
 }
